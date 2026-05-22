a95b8f9cb464a5720c7c25a0282ddf7e61c647d9
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-05-23 00:26:44 +0530

    Added Logic Building Assignment-3

---
+++ Assign-3/Prg1.c
@@ -0,0 +1,35 @@
+/*
+Q1. Write a program which accept one number from user and print that number of even numbers on screen.
+
+Input :
+7
+
+Output :
+2 4 6 8 10 12 14
+*/
+
+#include<stdio.h>
+
+void PrintEven(int iNo)
+{
+    int i = 0;
+    
+    if(iNo <= 0)
+    {
+        return ;
+    }
+
+    for(i = 1; i <= iNo; i++)
+    {
+        printf("%d\t",i * 2);
+    }
+}
+int main()
+{
+    int iValue = 0;
+    printf("Enter number: \n");
+    scanf("%d",&iValue);
+
+    PrintEven(iValue);
+    return 0;
+}
